fc847f41ffa09480b6ea87352f7dba9af409e632
vssanjana16 <vssanjana16@gmail.com>
2026-08-11 18:48:16 +0530

    Updated output

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String  args[]){
-        System.out.println("Continuous Integration Successful");
+        System.out.println("Continuous Integration Successful")
     }
 }
